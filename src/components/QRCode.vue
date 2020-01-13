4cf6871679918d2c21fa058335f4b941a271e65a
conan <conan@hacker.in.net>
2020-01-14 06:11:57 +0900

    qrcode

--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="QRCode">
+    <qriously :value="url" :size="200" />
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueQriously from 'vue-qriously'
+Vue.use(VueQriously)
+
 export default {
-  name: 'QRCode'
+  name: 'QRCode',
+  data: () => ({
+    url: "1234567"
+
+  })
 }
 </script>
