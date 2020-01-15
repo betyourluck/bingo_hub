157b647db2560e285cebdeb8d966dadf9ce845f5
conan <conan@hacker.in.net>
2020-01-16 00:22:47 +0900

    commit

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="Start">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <p>ゲーム作成ボタンを押してください</p>
+    <div class="my-2">
+      <v-btn x-large color="primary" dark>ゲームを作成する</v-btn>
+    </div>
     <QRCode/>
+    <UserList/>
+    <div class="my-2">
+      <v-btn x-large color="success" dark>ゲームを開始する</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import QRCode from '@/components/QRCode.vue'
+import UserList from '@/components/UserList.vue'
 
 export default {
   name: 'Start',
   components: {
-    QRCode
+    QRCode,
+    UserList
   }
 }
 </script>
